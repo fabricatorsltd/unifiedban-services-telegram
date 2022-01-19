6893e1e5abb8607b5793cb36b55077fbd313f2b7
Pietro di Caprio <pietro@fabricators.ltd>
2022-01-19 19:07:49 +0100

    Update license header, add LICENSE file

--- Commands/Command.cs
+++ Commands/Command.cs
@@ -3,15 +3,21 @@
 © fabricators SRL, https://fabricators.ltd , https://unifiedban.solutions
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the fabricator's FOSS License.
+it under the terms of the GNU Affero General Public License with our addition
+to Section 7 as published in unified/ban's the GitHub repository.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
-You should have received a copy of the fabricator's FOSS License
-along with this program.  If not, see <https://fabricators.ltd/FOSSLicense>. */
+You should have received a copy of the GNU Affero General Public License and the
+additional terms along with this program. 
+If not, see <https://docs.fabricators.ltd/docs/licenses/unifiedban>.
+
+For more information, see Licensing FAQ: 
+
+https://docs.fabricators.ltd/docs/licenses/faq */
 
 using System;
 using System.Collections.Generic;
